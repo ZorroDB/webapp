64b4abece445245de9692c6e1aa27a8b16d0c204
zorrodb <diegoblom01@gmail.com>
2024-05-20 14:17:37 +0200

    login

--- src/frontend/screens/loginScreen.jsx
+++ src/frontend/screens/loginScreen.jsx
@@ -1,10 +1,107 @@
-import React from 'react';
+import React, { useState } from "react";
+import './styling/login.css'
 
 const LoginScreen = () => {
+  const [errorMessages, setErrorMessages] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [formData, setFormData] = useState({
+    uname: "",
+    pass: ""
+  });
+
+  // Mock login validation
+  const database = [
+    {
+      username: "user1",
+      password: "pass1"
+    },
+    {
+      username: "user2",
+      password: "pass2"
+    }
+  ];
+
+  const errors = {
+    uname: "invalid username",
+    pass: "invalid password"
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const { uname, pass } = formData;
+
+    // Find user login info
+    const userData = database.find((user) => user.username === uname);
+
+    // Compare user info
+    if (userData) {
+      if (userData.password !== pass) {
+        // Invalid password
+        setErrorMessages({ name: "pass", message: errors.pass });
+      } else {
+        // Valid login
+        setIsSubmitted(true);
+      }
+    } else {
+      // Username not found
+      setErrorMessages({ name: "uname", message: errors.uname });
+    }
+  };
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  // Generate JSX code for error message
+  const renderErrorMessage = (name) =>
+    name === errorMessages.name && (
+      <div className="error">{errorMessages.message}</div>
+    );
+
   return (
-    <div>
-      <h1>Login</h1>
-      {/* Add your login form here */}
+    <div className="login-page">
+      <div className="header">
+      <h1>Log In</h1>
+      <span>Enter your email and password to log in to our dashboard</span>
+      </div>
+      <div className="login-form">
+        {isSubmitted ? (
+          <div>User is successfully logged in</div>
+        ) : (
+          <form onSubmit={handleSubmit}>
+            <div className="input-container">
+              <label>Email </label>
+              <input
+                type="text"
+                name="uname"
+                value={formData.uname}
+                onChange={handleInputChange}
+                required
+              />
+              {renderErrorMessage("uname")}
+            </div>
+            <div className="input-container">
+              <label>Password </label>
+              <input
+                type="password"
+                name="pass"
+                value={formData.pass}
+                onChange={handleInputChange}
+                required
+              />
+              {renderErrorMessage("pass")}
+              <span id="forgot_pwd">Forgot password?</span>
+            </div>
+            <div className="button-container">
+              <input type="submit" value="Log In" />
+            </div>
+          </form>
+        )}
+      </div>
     </div>
   );
 };
