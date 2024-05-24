673f59392c4cb7e354775a7f3adf315ef3a4ed60
zorrodb <diegoblom01@gmail.com>
2024-05-24 17:27:18 +0200

    update

--- src/backend/server.js
+++ src/backend/server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const bcrypt = require("bcrypt");
-const User = require("./models/User");
+const User = require("./models/Users");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
