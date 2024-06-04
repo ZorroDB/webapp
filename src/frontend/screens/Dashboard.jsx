56e32fd5e0b19e648168b89d43fd2332e0d9c88b
zorrodb <diegoblom01@gmail.com>
2024-06-04 12:11:38 +0200

    timer working

--- src/frontend/screens/Dashboard.jsx
+++ src/frontend/screens/Dashboard.jsx
@@ -2,38 +2,54 @@ import React from 'react';
 import './styling/dashboard.css';
 
 const Dashboard = () => {
-  let start = document.className('timer');
-  let stop = document.className('timer');
+  function padding(numPad) {
+    return numPad < 10 ? '0' + numPad : numPad.toString();
+  }
 
-  isTimer = false;
-  let timerId;
+  let hour = 0;
+  let minute = 0;
+  let second = 0;
 
-  start.addEventListener('click', function () {
-    if (isTimer == false) {
-      let timerId = setInterval(function () {
-        console.log('!');
-      }, 1000);
-    }
-  });
-  stop.addEventListener('click', function () {
-    if (isTimer == true) {
-      clearInterval(timerId);
-      console.log('Timer stopped!');
-    }
-  });
+  function increment() {
+    setInterval(() => {
+      second++;
+      if (second === 60) {
+        second = 0;
+        minute++;
+      }
+      if (minute === 60) {
+        minute = 0;
+        hour++;
+      }
+
+      // Update padded values
+      let padHour = padding(hour);
+      let padMinute = padding(minute);
+      let padSecond = padding(second);
+
+      // Update the HTML elements with the new time
+      document.getElementById('time_hour').innerHTML = padHour;
+      document.getElementById('time_minutes').innerHTML = padMinute;
+      document.getElementById('time_seconds').innerHTML = padSecond;
+    }, 1000);
+  }
+
   return (
     <div className="dashboard">
       <div>
         <h1>Dashboard</h1>
       </div>
       <button className="logout">Log Out</button>
-      <div className="timer" onClick={'StartTimer()'}>
+      <button className="timer" type="button" onClick={increment()}>
         <div className="time-management">
           <p>Click to start</p>
-          <span className="time-measure">0:00</span>
+          <div className="time-measure">
+            <span id="time_hour"></span>:<span id="time_minutes"></span>:
+            <span id="time_seconds"></span>
+          </div>
           <p>Clock in/ out</p>
         </div>
-      </div>
+      </button>
     </div>
   );
 };
