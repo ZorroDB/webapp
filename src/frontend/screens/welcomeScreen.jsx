53fdfd60eaddd08dc4e6a345915c84bccc212443
diego.blom <blomd9@hva.nl>
2024-11-13 14:58:16 +0100

    .

--- src/frontend/screens/welcomeScreen.jsx
+++ src/frontend/screens/welcomeScreen.jsx
@@ -1,24 +1,19 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./styling/landing.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './styling/landing.css';
 
 const WelcomePage = () => {
   return (
-    <div className="welcome-page">
+    <div className={StyleSheet.}>
       <h1>Clock in/ out application!</h1>
       <p>Please select an option:</p>
       <div className="options">
-        
         <Link to="/login" className="option">
-        <button id="login_btn">
-          Log In
-        </button>
+          <button id="login_btn">Log In</button>
         </Link>
-        
+
         <Link to="/register" className="option">
-        <button id="register_btn">
-          Register
-        </button>
+          <button id="register_btn">Register</button>
         </Link>
       </div>
     </div>
